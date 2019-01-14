3c2a07a7a33d064d255a483d9f24fdc884719021
Паровоз <parovoz@MacBook-Pro-Parovoz.local>
2019-01-14 11:46:36 +0300

    created a greeting file

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+import dialog from '..';
+
 console.log('Welcome to the Brain Games!');
+
+dialog();
---
+++ src/dialog.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+export default () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log('Hello, ' + userName + '!');
+};
+
+
+
