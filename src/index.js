3ecbb18a300d9e80ec1ae7e377a542a6414b6d6d
Паровоз <parovoz@MacBook-Pro-Parovoz.local>
2019-01-14 14:24:07 +0300

    add eslint

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 
 export default () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log('Hello, ' + userName + '!');
+  console.log(`Hello, ${userName}!`);
 };
