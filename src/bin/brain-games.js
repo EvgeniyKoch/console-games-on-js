3650c4c2f1c7fcd0c73bdfbc1dadc65b022e78af
Паровоз <parovoz@MacBook-Pro-Parovoz.local>
2019-01-15 10:04:35 +0300

    added brain-even

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import greet from '..';
+import { greet } from '..';
 
 console.log('Welcome to the Brain Games!');
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+export const greet = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
+export const questionGame = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no"');
+
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+
+  const attempts = 3;
+
+  const attemt = (num) => {
+    if (num === 0) {
+      return console.log(`Congratulations, ${name} !!`);
+    }
+
+    const randomNumber = Math.floor(Math.random() * 100);
+    const answer = readlineSync.question(`Question: ${randomNumber} `);
+    const correctAnswerNo = `"${answer}" is wrong answer ;(. Correct answer was "no".`;
+    const correctAnswerYes = `"${answer}" is wrong answer ;(. Correct answer was "yes".`;
+
+    if (randomNumber % 2 === 0 && answer === 'yes') {
+      console.log('Correct!');
+    } else if (randomNumber % 2 !== 0 && answer === 'yes') {
+      console.log(correctAnswerNo);
+      return console.log(`Let's try again, ${name}!`);
+    } else if (randomNumber % 2 === 0 && answer !== 'yes') {
+      return console.log(correctAnswerYes);
+    } else if (randomNumber % 2 !== 0 && answer === 'no') {
+      console.log('Correct!');
+    } else if (randomNumber % 2 === 0 && answer === 'no') {
+      console.log(correctAnswerYes);
+      return console.log(`Let's try again, ${name}!`);
+    } else if (randomNumber % 2 !== 0 && answer !== 'no') {
+      return console.log(correctAnswerNo);
+    }
+    return attemt(num - 1);
+  };
+
+  return attemt(attempts);
 };
