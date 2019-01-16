f275b3a25ec42257e029c641859a97c819c88752
Паровоз <parovoz@MacBook-Pro-Parovoz.local>
2019-01-16 19:18:14 +0300

    fix bags cod review

--- src/games/gameEngine.js
+++ src/games/gameEngine.js
@@ -14,15 +14,13 @@ export const nameRequest = () => {
 export const randomNumber = () => Math.floor(Math.random() * 100);
 export const question = val => readlineSync.question(`Question: ${val} `);
 export const questionAnswer = () => readlineSync.question('Your answer: ');
-export const tryAgain = name => console.log(`Let's try again, ${name}!`);
-export const correctMessege = () => console.log('Correct!!');
 export const isEven = num => num % 2 === 0;
 
 export const brainGame = (name, answer, answerCorrect) => {
   if (answerCorrect !== answer) {
     console.log(`"${answer}" is wrong answer ;(. Correct answer was "${answerCorrect}".`);
-    tryAgain(name);
+    console.log(`Let's try again, ${name}!`);
     return;
   }
-  correctMessege();
+  console.log('Correct!!');
 };
