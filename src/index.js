d32fc2e14ddd6b219c656da93c36f5f31852b93e
Паровоз <parovoz@MacBook-Pro-Parovoz.local>
2019-01-17 00:54:09 +0300

    add asciinema

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const brainGame = (descript, funcCorrectAnswer, question) => {
 
   const iter = (attempt) => {
     if (attempt === 0) {
-      console.log('Correct!!');
       console.log(`Congratulations, ${name} !!`);
       return;
     }
@@ -20,9 +19,6 @@ const brainGame = (descript, funcCorrectAnswer, question) => {
 
     const answerCorrect = funcCorrectAnswer(questionGame);
     const answer = readlineSync.question('Your answer: ');
-
-    console.log(typeof answer, 'typeof answer');
-    console.log(typeof answerCorrect, 'typeof answerCorrect');
 
     if (String(answerCorrect) !== answer) {
       console.log(`"${answer}" is wrong answer ;(. Correct answer was "${answerCorrect}".`);
