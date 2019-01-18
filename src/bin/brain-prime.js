21bb3ec7d0b8adbf975480cbd5b75b5993f40165
Паровоз <parovoz@MacBook-Pro-Parovoz.local>
2019-01-18 13:17:26 +0300

    added new game

---
+++ src/bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import brainPrimePlay from '../games/prime';
+
+brainPrimePlay();
---
+++ src/games/prime.js
@@ -0,0 +1,25 @@
+import brainGame from '..';
+import generateNum from '../utils';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const getGameData = () => {
+  const question = generateNum(0, 500);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+};
+
+export default () => brainGame(description, getGameData);
